827e942aefcd7908c86219041a187831e0a8fdaf
kimyubi <ieieie0419@gmail.com>
2020-12-23 17:59:58 +0900

    LeetCode_path-with-maximum-probability - solved

--- 다익스트라/LeetCode_path-with-maximum-probability.py
+++ 다익스트라/LeetCode_path-with-maximum-probability.py
@@ -1,29 +1,25 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
         graph = collections.defaultdict(list)
+        visited = set()
         
-        for x,y in zip(edges,succProb): # generate graph => start_v : (dst, probability)
-            graph[x[0]].append((x[1],float(y)))  
+        # generate graph
+        for (x,y),prob in zip(edges,succProb):
+            graph[x].append((y,prob))
+            graph[y].append((x,prob))
  
-            
-        Q = [(0,1,start)] # (dumy, probability, node)
-        # dumy는 probability가 큰 값을 우선 순위로 설정하기 위한 기준으로 쓰레기 값이다.
+        Q = [(0,1,start)] # (dumy,probability,node)
         
         while Q:
             dumy,probability,node = heapq.heappop(Q)
+            visited.add(node)
             
             if node == end:
-                return float(probability)
+                return probability
             
             for v,w in graph[node]:
-                alt = float(probability * w)
-                heapq.heappush(Q,(-alt,alt,v))  # probability가 큰 것을 우선
-                print(Q)
+                if v not in visited:
+                    alt = probability * w
+                    heapq.heappush(Q,(-alt,alt,v))
         
-        
-        return float(0)
-                
-# 테케 통과 / 제출 실패 - undirected weighted graph가 정점 간 양방향 이동 가능한 그래프인데 단방향 그래프처럼 풀었다.
-# 그럼 역방향도 그래프에 추가..? 양방향 무한 순회 문제 생각해보기.       
-            
-            
+        return 0
